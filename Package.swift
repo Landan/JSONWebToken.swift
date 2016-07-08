e02e7eb799d4f8f196781f1bf2b42f1b0b105a38
Sviatoslav <belmeha@gmail.com>
2016-07-08 23:03:01 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 let package = Package(
     name: "JWT",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 10),
-        .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/CryptoKitten/SHA2.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 13)
         ]
 )
