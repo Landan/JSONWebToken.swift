cf9dfa6f5035dbde9c30f6d9692655e94fd51a8b
Sviatoslav <belmeha@gmail.com>
2016-09-18 01:17:25 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "JWT",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 13)
+       
         ]
 )
